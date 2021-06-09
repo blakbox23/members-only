7984a41ac182d8947bebd24fbcdfd788a049bf49
blakbox23 <boudii30@gmail.com>
2021-06-09 18:13:54 +0300

    Add posts controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  devise_for :users
+  
+  root to: "posts#index"
+
+  resources :posts, only: [:new, :create, :index] 
+
+
 end
