107fc87eb29a6a24f96dd97101492d55bb0abc36
blakbox23 <boudii30@gmail.com>
2021-06-09 19:00:20 +0300

    Add create action and modify index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        @post = Post.new(post_params)
+        if@post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :body).merge(user: current_user)
+    end
+    
 end
