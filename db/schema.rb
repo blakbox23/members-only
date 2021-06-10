b28355ef11e5c5b5f0c1c6ca5be38e7eb08505c3
Deepakdanger <depk.96.kumar@gmail.com>
2021-06-10 14:32:58 +0530

    Modify logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_150148) do
+ActiveRecord::Schema.define(version: 2021_06_10_090014) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
